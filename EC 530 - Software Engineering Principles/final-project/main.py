c7f0c472cb05e94b83fae388baf9b5a54192568b
Dylan Ramdhan <114676339+dylanramdhan@users.noreply.github.com>
2025-04-28 22:43:03 -0400

    updating main.py

---
+++ EC 530 - Software Engineering Principles/final-project/main.py
@@ -0,0 +1,10 @@
+"""
+   Final Project for EC530
+   Engineer: Dylan Ramdhan
+
+   Welcome to Grade Mate!
+   
+   This program is designed to help professors manage their grades and assignments. 
+   It allows professors to create assignments, add students, and calculate final grades.
+
+"""
